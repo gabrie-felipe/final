ec355468c875265431931a6bb42bcbc98ce6ff47
Gabriel Felipe <gabriel.souza16@alunos.sc.senac.br>
2023-02-16 17:00:42 -0300

    finish

---
+++ src/lib/styles.jsx
@@ -0,0 +1,36 @@
+import { StyleSheet } from "react-native";
+
+
+export const styles = StyleSheet.create({
+    container: {
+        width: "100%",
+        alignItems: "center",
+        marginBottom: 100,
+        paddingHorizontal: 200,
+        flex: 1,
+        justifyContent: "center",
+        backgroundColor: "#dce0e6"
+    },
+    text: {
+      fontSize: 30,
+      fontWeight: "bold",
+      color: "#606d80",
+   
+    },
+    paragraph:{
+        fontSize: 20,
+        fontWeight: "bold",
+        color: "#1f1f20",
+        marginBottom: 40,
+    
+      },
+      titulo:{
+        fontSize: 30,
+        fontWeight: "bold",
+        color: "#2b4c7e",
+        marginBottom: 20,
+      },
+     digite:{
+        marginBottom: 20,
+     }
+  });
