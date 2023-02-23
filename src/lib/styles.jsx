4772ae1eed96244dfdf9adffeaaa2860bf8bb942
Gabriel Felipe <gabriel.souza16@alunos.sc.senac.br>
2023-02-23 15:29:50 -0300

    add melhorias

--- src/lib/styles.jsx
+++ src/lib/styles.jsx
@@ -1,36 +1,37 @@
 import { StyleSheet } from "react-native";
 
-
 export const styles = StyleSheet.create({
-    container: {
-        width: "100%",
-        alignItems: "center",
-        marginBottom: 100,
-        paddingHorizontal: 200,
-        flex: 1,
-        justifyContent: "center",
-        backgroundColor: "#dce0e6"
-    },
-    text: {
-      fontSize: 30,
-      fontWeight: "bold",
-      color: "#606d80",
-   
-    },
-    paragraph:{
-        fontSize: 20,
-        fontWeight: "bold",
-        color: "#1f1f20",
-        marginBottom: 40,
-    
-      },
-      titulo:{
-        fontSize: 30,
-        fontWeight: "bold",
-        color: "#2b4c7e",
-        marginBottom: 20,
-      },
-     digite:{
-        marginBottom: 20,
-     }
-  });
+  container: {
+    flex: 1,
+    alignContent: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#73b06f",
+  },
+  text: {
+    fontSize: 5,
+    fontWeight: "bold",
+    color: "#405059",
+  },
+  paragraph: {
+    fontSize: 25,
+    fontWeight: "bold",
+    color: "#405059",
+    flex: 1,
+    alignContent: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    width: 400,
+    marginBottom: 0,
+  },
+  titulo: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "#2b4c7e",
+    marginBottom: 60,
+  },
+
+  box: {
+    marginBottom: 20,
+  },
+});
